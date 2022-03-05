5bab732d5025965e2871aca97a55a78604d03d63
Julian <julian.tarczynski2000@gmail.com>
2022-03-05 22:49:36 +0100

    initial commit

---
+++ Backend/Book-module/src/main/java/com/example/Bookmodule/BookModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bookmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookModuleApplication.class, args);
+	}
+
+}
---
+++ Backend/Book-module/src/test/java/com/example/Bookmodule/BookModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bookmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
